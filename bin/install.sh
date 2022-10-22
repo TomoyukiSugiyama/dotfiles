946eb9630fa1609451bffe9fc9d9ce4e2ca25c9b
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-10-22 23:09:20 +0900

    update help message

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -ue
 
 help() {
-  command echo "Usage: $0 [--help | -h]" 0>&2
-  command echo ""
+  command echo "Usage:"
+  command echo "    $(basename ${0}) [--help | -h]" 0>&2
+  command echo "Options:"
+  command echo "    --help, -h        help message"
 }
 
 while [ $# -gt 0 ];do
