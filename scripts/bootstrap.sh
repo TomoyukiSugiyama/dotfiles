5f06be43f93f19503ce7fccec38989d432ba6037
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-10-24 10:50:27 +0900

    rename

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -ue
+
+help() {
+  command echo "Usage:"
+  command echo "    $(basename ${0}) [--help | -h]" 0>&2
+  command echo "Options:"
+  command echo "    --help, -h        help message"
+}
+
+link() {
+  command echo "Create backup directory for old dotfiles..."
+  if [ ! -d "$HOME/.dotbackup" ];then
+    command echo "$HOME/.dotbackup not found. Generate .dotbackup directory."
+    command mkdir "$HOME/.dotbackup"
+  fi
+
+  local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+  local dotdir=$(dirname ${script_dir})
+
+  command echo "Start to link dotfiles to the home directory."
+  if [[ "$HOME" != "$dotdir" ]];then
+    for f in $dotdir/.??*; do
+      [[ `basename $f` == ".git" ]] && continue
+      if [[ -L "$HOME/`basename $f`" ]];then
+        # command rm -f "$HOME/`basename $f`"
+        echo "command rm -f $HOME/`basename $f`"
+      fi
+      if [[ -e "$HOME/`basename $f`" ]];then
+        # command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
+        echo "command mv $HOME/`basename $f` $HOME/.dotbackup"
+      fi
+      #command ln -snf $f $HOME
+      echo "command ln -snf $f $HOME"
+    done
+  else
+    command echo "Home and dotfiles are same direrctory. Please change directory."
+  fi
+}
+
+while [ $# -gt 0 ];do
+  case ${1} in
+    --help|-h)
+      help
+      exit 1
+      ;;
+    *)
+      ;;
+  esac
+  shift
+done
+
+link
