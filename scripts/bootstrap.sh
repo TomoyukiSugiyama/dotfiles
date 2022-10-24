5d369c7063a6f609efdb6533e58c6877f0719042
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-10-24 15:32:20 +0900

    fix indent

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,42 +1,40 @@
 #!/bin/bash -ue
 
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+dotdir=$(dirname "${script_dir}")
+
 function help() {
-  command echo "Usage:"
-  command echo "    $(basename "${0}") [--help | -h]" 0>&2
-  command echo "Options:"
-  command echo "    --help, -h        help message"
+    command echo "Usage:"
+    command echo "    $(basename "${0}") [--help | -h]" 0>&2
+    command echo "Options:"
+    command echo "    --help, -h        help message"
 }
 
 function link() {
-  command echo "Create backup directory for old dotfiles..."
-  if [ ! -d "${HOME}/.dotbackup" ];then
-    command echo "${HOME}/.dotbackup not found. Generate .dotbackup directory."
-    command mkdir "${HOME}/.dotbackup"
-  fi
+    command echo "Create backup directory for old dotfiles..."
+    if [ ! -d "${HOME}/.dotbackup" ];then
+        command echo "${HOME}/.dotbackup not found. Generate .dotbackup directory."
+        command mkdir "${HOME}/.dotbackup"
+    fi
 
-  local script_dir
-  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-  local dotdir
-  dotdir=$(dirname "${script_dir}")
-
-  command echo "Start to link dotfiles to the home directory."
-  if [[ "${HOME}" != "${dotdir}" ]];then
-    for f in "${dotdir}"/.??*; do
-      [[ $(basename "$f") == ".git" ]] && continue
-      if [[ -L "${HOME}/$(basename "$f")" ]];then
-        # command rm -f "${HOME}/$(basename "$f")"
-        echo "command rm -f ${HOME}/$(basename "$f")"
-      fi
-      if [[ -e "${HOME}/$(basename "$f")" ]];then
-        # command mv "${HOME}/$(basename "$f")" "${HOME}/.dotbackup"
-        echo "command mv ${HOME}/$(basename "$f") ${HOME}/.dotbackup"
-      fi
-      #command ln -snf $f ${HOME}
-      echo "command ln -snf $f ${HOME}"
-    done
-  else
-    command echo "Home and dotfiles are same directory. Please change directory."
-  fi
+    command echo "Start to link dotfiles to the home directory."
+    if [[ "${HOME}" != "${dotdir}" ]];then
+        for f in "${dotdir}"/.??*; do
+            [[ $(basename "$f") == ".git" ]] && continue
+            if [[ -L "${HOME}/$(basename "$f")" ]];then
+                # command rm -f "${HOME}/$(basename "$f")"
+                echo "command rm -f ${HOME}/$(basename "$f")"
+            fi
+            if [[ -e "${HOME}/$(basename "$f")" ]];then
+                # command mv "${HOME}/$(basename "$f")" "${HOME}/.dotbackup"
+                echo "command mv ${HOME}/$(basename "$f") ${HOME}/.dotbackup"
+            fi
+            #command ln -snf $f ${HOME}
+            echo "command ln -snf $f ${HOME}"
+        done
+    else
+        command echo "Home and dotfiles are same directory. Please change directory."
+    fi
 }
 
 function setup_brew() {
@@ -44,24 +42,23 @@ function setup_brew() {
         command echo "Homebrew is not found in your local pc. Begin to install homebrew."
         curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
     fi
-
-    brew analytics off
-    brew cleanup --prune=all
-    brew upgrade
+    # brew analytics off
+    # brew cleanup --prune=all
+    # brew upgrade
     # brew bundle --file "$HOME/dotfiles/Brewfile"
     # brew doctor || true    
 }
 
 while [ $# -gt 0 ];do
-  case ${1} in
-    --help|-h)
-      help
-      exit 1
-      ;;
-    *)
-      ;;
-  esac
-  shift
+    case ${1} in
+        --help|-h)
+            help
+            exit 1
+            ;;
+        *)
+            ;;
+    esac
+    shift
 done
 
 setup_brew
