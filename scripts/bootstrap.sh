a1cb1e19ea07f9d28c8181845cbfa1af31ccfbbc
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-10-24 12:44:25 +0900

    refactoring script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,7 +2,7 @@
 
 function help() {
   command echo "Usage:"
-  command echo "    $(basename ${0}) [--help | -h]" 0>&2
+  command echo "    $(basename "${0}") [--help | -h]" 0>&2
   command echo "Options:"
   command echo "    --help, -h        help message"
 }
@@ -14,20 +14,22 @@ function link() {
     command mkdir "${HOME}/.dotbackup"
   fi
 
-  local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-  local dotdir=$(dirname ${script_dir})
+  local script_dir
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+  local dotdir
+  dotdir=$(dirname "${script_dir}")
 
   command echo "Start to link dotfiles to the home directory."
   if [[ "${HOME}" != "${dotdir}" ]];then
-    for f in ${dotdir}/.??*; do
-      [[ `basename $f` == ".git" ]] && continue
-      if [[ -L "${HOME}/`basename $f`" ]];then
-        # command rm -f "${HOME}/`basename $f`"
-        echo "command rm -f ${HOME}/`basename $f`"
+    for f in "${dotdir}"/.??*; do
+      [[ $(basename "$f") == ".git" ]] && continue
+      if [[ -L "${HOME}/$(basename "$f")" ]];then
+        # command rm -f "${HOME}/$(basename "$f")"
+        echo "command rm -f ${HOME}/$(basename "$f")"
       fi
-      if [[ -e "${HOME}/`basename $f`" ]];then
-        # command mv "${HOME}/`basename $f`" "${HOME}/.dotbackup"
-        echo "command mv ${HOME}/`basename $f` ${HOME}/.dotbackup"
+      if [[ -e "${HOME}/$(basename "$f")" ]];then
+        # command mv "${HOME}/$(basename "$f")" "${HOME}/.dotbackup"
+        echo "command mv ${HOME}/$(basename "$f") ${HOME}/.dotbackup"
       fi
       #command ln -snf $f ${HOME}
       echo "command ln -snf $f ${HOME}"
