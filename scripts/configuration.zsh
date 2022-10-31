90b55be2fee0deaf290bb972ce523c0f845a4396
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-10-31 19:22:34 +0900

    delete unused code

--- scripts/configuration.zsh
+++ scripts/configuration.zsh
@@ -6,19 +6,7 @@ script_dir="$(cd "$(dirname "${(%):-%N}")" && pwd -P)"
 dotdir=$(dirname "${script_dir}")
 
 function link() {
-
-    echo "Create backup directory for old dotfiles..."
-
-    if [ ! -d "${HOME}/.dotbackup" ];then
-        echo "${HOME}/.dotbackup not found. Generate .dotbackup directory."
-        mkdir "${HOME}/.dotbackup"
-    fi
-
     echo "Start to link dotfiles to the home directory."
-    if [[ "${HOME}" == "${dotdir}" ]];then
-        echo "[Error] Home directory and dotfiles directory are same path. Please change your home or dotfiles directory path."
-        exit 1
-    fi
 
     # git
     ln -fs "${dotdir}/git/.gitignore" "${HOME}/.gitignore"
