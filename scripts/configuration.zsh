d446a2704594be55972658d1a6692686915f89dd
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-10-31 19:25:06 +0900

    set help message

--- scripts/configuration.zsh
+++ scripts/configuration.zsh
@@ -5,6 +5,13 @@ set -euo pipefail
 script_dir="$(cd "$(dirname "${(%):-%N}")" && pwd -P)"
 dotdir=$(dirname "${script_dir}")
 
+function help() {
+    echo "Usage:"
+    echo "    $(basename "${0}") [--help | -h]" 0>&2
+    echo "Options:"
+    echo "    --help, -h        help message"
+}
+
 function link() {
     echo "Start to link dotfiles to the home directory."
 
@@ -20,5 +27,17 @@ function link() {
     ln -fs "${dotdir}/zsh/.zshrc" "${HOME}/.zshrc"
     source "${HOME}/.zshrc"
 }
+
+while [ $# -gt 0 ];do
+    case ${1} in
+        --help|-h)
+            help
+            exit 1
+            ;;
+        *)
+            ;;
+    esac
+    shift
+done
 
 link
