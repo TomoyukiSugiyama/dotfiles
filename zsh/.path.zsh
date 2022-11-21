a59f2a69965e4200e2c8e0bccf058527492de3ee
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-11-21 14:10:27 +0900

    Delete google cloud sdk from brewfile (#42)

--- zsh/.path.zsh
+++ zsh/.path.zsh
@@ -1,4 +1,8 @@
 #!/usr/bin/env zsh
+# shellcheck disable=SC1091
 
 export PATH="$PATH:/opt/homebrew/share/git-core/contrib/diff-highlight"
 export PATH="/usr/local/sbin:$PATH"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "${HOME}/google-cloud-sdk/path.zsh.inc" ]; then . "${HOME}/google-cloud-sdk/path.zsh.inc"; fi
