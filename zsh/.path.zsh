a9f0ce48b0e9f5c537688a38a98df40f2a482c60
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-11-24 20:37:11 +0900

    change indent

--- zsh/.path.zsh
+++ zsh/.path.zsh
@@ -5,4 +5,6 @@ export PATH="$PATH:/opt/homebrew/share/git-core/contrib/diff-highlight"
 export PATH="/usr/local/sbin:$PATH"
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f "${HOME}/google-cloud-sdk/path.zsh.inc" ]; then . "${HOME}/google-cloud-sdk/path.zsh.inc"; fi
+if [ -f "${HOME}/google-cloud-sdk/path.zsh.inc" ]; then
+    . "${HOME}/google-cloud-sdk/path.zsh.inc"
+fi
