21c674e0305d433ec59a4fed8953a5909fd330cc
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-11-26 09:55:26 +0900

    Brew google cloud sdk (#46)

--- zsh/.path.zsh
+++ zsh/.path.zsh
@@ -5,6 +5,6 @@ export PATH="$PATH:/opt/homebrew/share/git-core/contrib/diff-highlight"
 export PATH="/usr/local/sbin:$PATH"
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f "${HOME}/google-cloud-sdk/path.zsh.inc" ]; then
-    . "${HOME}/google-cloud-sdk/path.zsh.inc"
+if [ -f "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc" ]; then
+    source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
 fi
