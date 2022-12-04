c7ee332cd21cf4cacb19f5c84f1ee330431bf871
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-12-04 22:08:51 +0900

    delete help

--- scripts/bootstrap.zsh
+++ scripts/bootstrap.zsh
@@ -7,13 +7,6 @@ script_path="${(%):-%N}"
 script_dir="$(cd "$(dirname "${script_path}")" && pwd -P)"
 dotdir=$(dirname "${script_dir}")
 
-function help() {
-    echo "Usage:"
-    echo "    ${script_path} [--help | -h]" 0>&2
-    echo "Options:"
-    echo "    --help, -h        help message"
-}
-
 function setup_brew() {
     echo "setup brew"
     if ! (type brew > /dev/null 2>&1); then
@@ -29,17 +22,5 @@ function setup_brew() {
     brew bundle --file "${dotdir}/Brewfile"
     brew doctor || true
 }
-
-while [ $# -gt 0 ];do
-    case ${1} in
-        --help|-h)
-            help
-            exit 1
-            ;;
-        *)
-            ;;
-    esac
-    shift
-done
 
 setup_brew
