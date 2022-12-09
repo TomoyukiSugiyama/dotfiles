9b474381cd956a04985b9d1b9463d7899063e696
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-12-09 09:50:19 +0900

    update prompt

--- zsh/.terminal.zsh
+++ zsh/.terminal.zsh
@@ -22,7 +22,7 @@ zstyle ':vcs_info:*' actionformats '%F{red}(%s)-[%b|%a]%f'
  
 function _update_vcs_info_msg() {
     LANG=en_US.UTF-8 vcs_info
-    PROMPT="${vcs_info_msg_0_} "$PROMPT
+    RPROMPT="${vcs_info_msg_0_}"
 }
 
 add-zsh-hook precmd _update_vcs_info_msg
