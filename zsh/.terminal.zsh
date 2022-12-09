425cef194c55769e59e18d136f1b30e195ef1549
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-12-09 09:51:46 +0900

    Update terminal (#54)

--- zsh/.terminal.zsh
+++ zsh/.terminal.zsh
@@ -10,27 +10,30 @@ TERM=xterm-256color
 autoload -Uz colors
 colors
 
-PROMPT="%F{blue}[%m][%n]:%f %c/ %# "
+PROMPT="%F{blue}[%m][%n]:%f %c/ 
+%# "
+
+# vcs_info
+autoload -Uz vcs_info
+autoload -Uz add-zsh-hook
+ 
+zstyle ':vcs_info:*' formats '%F{green}(%s)-[%b]%f'
+zstyle ':vcs_info:*' actionformats '%F{red}(%s)-[%b|%a]%f'
+ 
+function _update_vcs_info_msg() {
+    LANG=en_US.UTF-8 vcs_info
+    RPROMPT="${vcs_info_msg_0_}"
+}
+
+add-zsh-hook precmd _update_vcs_info_msg
 
 if [ -f "$(brew --prefix)/share/kube-ps1.sh" ]; then 
     ## if you want to customize, please edit `~/.kube/config`
     set +u
+    KUBE_PS1_SYMBOL_USE_IMG=true
     source "$(brew --prefix)/share/kube-ps1.sh"
     PS1='$(kube_ps1) '$PS1
 fi
-
-# vcs_info
-# autoload -Uz vcs_info
-# autoload -Uz add-zsh-hook
- 
-# zstyle ':vcs_info:*' formats '%F{green}(%s)-[%b]%f'
-# zstyle ':vcs_info:*' actionformats '%F{red}(%s)-[%b|%a]%f'
- 
-# function _update_vcs_info_msg() {
-#     LANG=en_US.UTF-8 vcs_info
-#     RPROMPT="${vcs_info_msg_0_}"
-# }
-# add-zsh-hook precmd _update_vcs_info_msg
 
 # setopt
 setopt no_beep
