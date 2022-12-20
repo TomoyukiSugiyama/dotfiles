dc699d0eb2328857b3d8df5af71de6f3ae065a85
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-12-20 12:49:12 +0900

    add comment

--- zsh/.path.zsh
+++ zsh/.path.zsh
@@ -3,9 +3,11 @@
 
 export PATH="$PATH:/opt/homebrew/share/git-core/contrib/diff-highlight"
 export PATH="/usr/local/sbin:$PATH"
-export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc" ]; then
     source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
 fi
+
+# gcloud.
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
