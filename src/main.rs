1ab302ef968c45460462f482e3ca6d1cf0890a56
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2025-10-17 19:55:31 +0900

    Refactor application structure and enhance menu functionality in main.rs
    - Introduced a Menu struct to manage menu items and their state.
    - Updated the App struct to include the menu and initialized it with options.
    - Enhanced the rendering logic to display a list of menu items with selection capabilities.
    - Implemented key handlers for navigating the menu, including selecting the first, last, previous, and next items.
    - Removed the previous log display in the render method for a cleaner UI.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use color_eyre::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use ratatui::{
-    DefaultTerminal, Frame,
-    style::Stylize,
-    text::Line,
-    widgets::{Block, Paragraph},
+    DefaultTerminal,
+    layout::Rect,
+    widgets::{Block, List, ListItem, ListState, StatefulWidget, HighlightSpacing, Widget, Borders},
+    symbols,
+    style::{Style, Modifier},
+    buffer::Buffer,
+    style::{palette::tailwind::{SLATE, BLUE},},
 };
 use std::process::Command;
 
@@ -18,23 +21,54 @@ fn main() -> color_eyre::Result<()> {
 
 /// The main application which holds the state and logic of the application.
 #[derive(Debug, Default)]
-pub struct App {
+struct App {
     /// Is the application running?
     running: bool,
-    log: String,
+    menu: Menu,
 }
 
+#[derive(Debug, Default)]
+struct Menu {
+    state: ListState,
+    items: Vec<MenuItem>,
+}
+
+#[derive(Debug, Default)]
+struct MenuItem {
+    title: String,
+}
+
+impl FromIterator<String> for Menu {
+    fn from_iter<T>(iter: T) -> Self where T: IntoIterator<Item = String> {
+        let items = iter.into_iter().map(|title| MenuItem { title: title }).collect();
+        Self { items: items, state: ListState::default() }
+    }
+}
+
+const SELECTED_STYLE: Style = Style::new().bg(SLATE.c800).add_modifier(Modifier::BOLD);
+
+impl Widget for &mut App {
+    fn render(self, area: Rect, buffer: &mut Buffer) {
+        self.render(area, buffer);
+    }
+}
 impl App {
     /// Construct a new instance of [`App`].
     pub fn new() -> Self {
-        Self::default()
+        Self {
+            running: true,
+            menu: Menu::from_iter([
+                "Update Dotfiles".to_string(),
+                "Quit".to_string(),
+            ]),
+        }
     }
 
     /// Run the application's main loop.
     pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
         self.running = true;
         while self.running {
-            terminal.draw(|frame| self.render(frame))?;
+            terminal.draw(|frame| frame.render_widget(&mut self, frame.area()))?;
             self.handle_crossterm_events()?;
         }
         Ok(())
@@ -46,21 +80,25 @@ impl App {
     ///
     /// - <https://docs.rs/ratatui/latest/ratatui/widgets/index.html>
     /// - <https://github.com/ratatui/ratatui/tree/main/ratatui-widgets/examples>
-    fn render(&mut self, frame: &mut Frame) {
-        let title = Line::from("Dotfiles Manager")
-            .bold()
-            .blue()
-            .centered();
-        let text = "Hello, Dotfiles!\n\n\
-            Created using https://github.com/tomoyukisugiyama/dotfiles\n\
-            Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
-            Press `u` to update dotfiles.\n\n";
+    fn render(&mut self, area: Rect, buffer: &mut Buffer) {
+        // let title = Line::from("Dotfiles Manager").bold().blue().centered();
+        // let text = "Hello, Dotfiles!\n\n\
+        //     Created using https://github.com/tomoyukisugiyama/dotfiles\n\
+        //     Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
+        //     Press `u` to update dotfiles.\n\n";
 
-        frame.render_widget(
-            Paragraph::new(text.to_string() + &self.log.clone())
-                .block(Block::bordered().title(title)),
-            frame.area(),
-        );
+        let block = Block::new()
+        .title("Dotfiles Manager")
+        .borders(Borders::TOP)
+        .border_set(symbols::border::EMPTY)
+        .border_style(Style::new().fg(SLATE.c100).bg(BLUE.c800));
+        let items = self.menu.items.iter().map(|item| ListItem::new(item.title.clone())).collect::<Vec<ListItem>>();
+        let list = List::new(items)
+        .block(block)
+        .highlight_style(SELECTED_STYLE)
+        .highlight_symbol("> ")
+        .highlight_spacing(HighlightSpacing::Always);
+        StatefulWidget::render(list, area, buffer, &mut self.menu.state);
     }
 
     /// Reads the crossterm events and updates the state of [`App`].
@@ -85,8 +123,28 @@ impl App {
             | (KeyModifiers::CONTROL, KeyCode::Char('c') | KeyCode::Char('C')) => self.quit(),
             // Add other key handlers here.
             (_, KeyCode::Char('u')) => self.update_dotfiles(),
+            (_, KeyCode::Home) => self.select_first(),
+            (_, KeyCode::End) => self.select_last(),
+            (_, KeyCode::Up) => self.select_previous(),
+            (_, KeyCode::Down) => self.select_next(),
             _ => {}
         }
+    }
+
+    fn select_first(&mut self) {
+        self.menu.state.select_first();
+    }
+
+    fn select_last(&mut self) {
+        self.menu.state.select_last();
+    }
+
+    fn select_previous(&mut self) {
+        self.menu.state.select_previous();
+    }
+
+    fn select_next(&mut self) {
+        self.menu.state.select_next();
     }
 
     /// Set running to false to quit the application.
@@ -95,17 +153,12 @@ impl App {
     }
 
     fn update_dotfiles(&mut self) {
-        self.log = "Updating dotfiles...\n".to_string();
-
         let output = Command::new("git")
             .arg("pull")
             .arg("-r")
             .arg("--autostash")
             .output()
             .expect("Failed to update dotfiles");
-        self.log = String::from_utf8_lossy(&output.stdout).to_string();
-        self.log += "\n";
-        self.log += &String::from_utf8_lossy(&output.stderr).to_string();
-        self.log += "\n";
+        println!("output: {:?}", output);
     }
 }
